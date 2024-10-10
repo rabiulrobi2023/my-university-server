532c82b09be5600bc01d2452efe33310f1100234
Rabiul Islam <rabiul.robi2023@gmail.com>
2024-10-10 12:12:57 +0600

    Default Validation Implemented in student.model.ts

--- src/app/modules/student/student.model.ts
+++ src/app/modules/student/student.model.ts
@@ -6,29 +6,79 @@ import {
   TUserName,
 } from './student.interface';
 
+//=========================User/Student Name Schema=====================================
 export const userNameSchema = new Schema<TUserName>({
-  firstName: { type: String, required: true },
-  middleName: { type: String, required: true },
-  lastName: { type: String, required: true },
+  firstName: {
+    type: String,
+    trim: true,
+    maxlength: [10, 'First name should be within 10 character'],
+    required: [true, 'First name is required'],
+  },
+  middleName: {
+    type: String,
+    trim: true,
+    maxlength: [10, 'Mid name should be within 10 character'],
+    required: [true, 'Middle name is required'],
+  },
+  lastName: {
+    type: String,
+    maxlength: [10, 'Last name should be within 10 character'],
+    required: [true, 'Last name is required'],
+  },
 });
 
+//=========================Guardian Schema=====================================
 export const guardianSchema = new Schema<TGuardian>({
-  fathersName: { type: String, required: true },
-  fathersOccupation: { type: String, required: true },
-  fathersContactNo: { type: String, required: true },
-  mothersName: { type: String, required: true },
-  mothersOccupation: { type: String, required: true },
-  mothersContactNo: { type: String, required: true },
+  fathersName: { type: String, required: [true, 'First name is required'] },
+  fathersOccupation: {
+    type: String,
+    required: [true, "Father's occupation is required"],
+  },
+  fathersContactNo: {
+    type: String,
+    required: [true, "Father's contact no is required"],
+  },
+  mothersName: { type: String, required: [true, "Mother's Name is required"] },
+  mothersOccupation: {
+    type: String,
+    required: [true, "Mother's occupation is required"],
+  },
+  mothersContactNo: {
+    type: String,
+    required: [true, "Mother's contact no is required"],
+  },
 });
 
+
+//=========================Local Guardian  Schema=====================================
 export const localGuardianSchema = new Schema<TLocalGuardian>({
-  loacalGuardianName: { type: String, required: true },
-  localGuardianAddress: { type: String, required: true },
-  localGuardianContactNo: { type: String, required: true },
+  loacalGuardianName: {
+    type: String,
+    required: [true, 'Local guardian is required'],
+  },
+  localGuardianAddress: {
+    type: String,
+    required: [true, 'Local guardian adddress is required'],
+  },
+  localGuardianContactNo: {
+    type: String,
+    required: [true, 'Contact no of local guardian is required'],
+  },
 });
 
+
+
+//=========================MainSchema=====================================
 const studentSchema = new Schema<TStudent>({
-  id: { type: String, required: true, unique: true },
+  id: {
+    type: String,
+    trim: true,
+    maxlength: [6, 'ID should be maximum length 6'],
+    minlength: [6, 'ID should be minimum length 6'],
+    required: [true, 'ID is required'],
+
+    unique: true,
+  },
   name: { type: userNameSchema, required: true },
   gender: {
     type: String,
@@ -36,23 +86,32 @@ const studentSchema = new Schema<TStudent>({
       values: ['Male', 'Female', 'Others'],
       message: '{VALUE} is not supported',
     },
-    required: true,
+    required: [true, 'Gender is required'],
   },
-  dateOfBirth: { type: Date, required: true },
+  dateOfBirth: { type: Date, required: [true, 'DOB is required'] },
   bloodGroup: {
     type: String,
     enum: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
     required: true,
   },
-  email: { type: String, required: true },
-  contactNo: { type: String, required: true },
-  emergencyContactNo: { type: String },
-  presentAddress: { type: String, required: true },
-  permanentAddress: { type: String, required: true },
+  email: { type: String, required: [true, 'Email address is required'] },
+  contactNo: { type: String, required: [true, 'Contact no is required'] },
+  emergencyContactNo: {
+    type: String,
+    required: [true, 'Emergency contact no is required'],
+  },
+  presentAddress: {
+    type: String,
+    required: [true, 'Present address is required'],
+  },
+  permanentAddress: {
+    type: String,
+    required: [true, 'Permanent address is required'],
+  },
   guardian: { type: guardianSchema, required: true },
   localGuardian: {
     type: localGuardianSchema,
-    required: [true, 'Local guardian is required'],
+    required: true,
   },
   profileImage: { type: String },
   status: {
@@ -60,7 +119,7 @@ const studentSchema = new Schema<TStudent>({
     enum: ['active', 'inActive'],
     default: 'active',
   },
-  isDeleted: { type: Boolean, required: true },
+  isDeleted: { type: Boolean, default: false },
   createdAt: { type: Date, required: true },
   updatedAt: { type: Date, required: true },
 });
